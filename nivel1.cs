edc6f52814418728279b5bc477de50072973b389
Cristian Zuniga <cristianzunigacordova@gmail.com>
2023-06-26 21:56:47 -0600

    subiendo nivel1

--- nivel1.cs
+++ nivel1.cs
@@ -2,6 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using XMLComentario;
 
-Mensajes m = new();
+    Mensajes m = new();
 
 m.MostrarRespueta("xd");
+
+public class nivel1 
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
